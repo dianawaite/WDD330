b11ac8843b7b79cdfcb01abc78484745e641a061
dianawaite <wai19007@byui.edu>
2022-09-24 22:06:48 -0600

    week 2 updates

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var lastTampering = document.lastModified;
+document.getElementById("output").textContent =
+"Last Updated: " + lastTampering;
+
+var currentYear = new Date();
+document.getElementById("classYear").textContent = currentYear.getFullYear();
+
+const links = [
+    {
+        label: "Week01 Notes",
+        url: "week01/notes.html"
+    }
+    
+];
+document.getElementById("toclist").innerHTML = links.join("");
+
+
+
+
+"<li>Week 1: <a href='/week01/notes.html'>Notes</a></li>"
